6f8875ee12ccb4e026ab2c94cd5cc48e20326224
Jason Hoffman <jason.michael.hoffman@gmail.com>
2019-08-19 13:19:23 -0700

    ASP.NET exercises from channel9.msdn.com. first commit

---
+++ myConsoleApp/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace myConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
